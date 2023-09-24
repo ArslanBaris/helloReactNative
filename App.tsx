fe89f124156d26d7d32cfab068ab6de7db473e51
ArslanBaris <arslan.bariss@outlook.com>
2023-09-24 20:50:49 +0300

    FlatList - 1

--- App.tsx
+++ App.tsx
@@ -22,6 +22,7 @@ import {
   TouchableOpacity,
   TextInput,
   Dimensions,
+  FlatList,
 } from 'react-native';
 
 import {
@@ -31,48 +32,33 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
-const {width}  = Dimensions.get('window');
+import data from './data';
 
 
 function App(): JSX.Element {
 
 
+  const renderContactsItem = ({item,index}:any) =>{
+    return(
+      <View>
+      <Text>{item.name}</Text>
+    </View>
+    )
+  }
+
   return (
     <View style={styles.container}>
-    <ScrollView
-      pagingEnabled={true}
-      horizontal={true}
-    >
-      <Text style={styles.title}>1</Text>
-      <Text style={styles.title}>2</Text>
-      <Text style={styles.title}>3</Text>
-      <Text style={styles.title}>4</Text>
-      <Text style={styles.title}>5</Text>
-      <Text style={styles.title}>6</Text>
-      <Text style={styles.title}>7</Text>
-      <Text style={styles.title}>8</Text>
-      <Text style={styles.title}>9</Text>
-      <Text style={styles.title}>10</Text>
-    </ScrollView>
+    	<FlatList
+					renderItem={renderContactsItem}
+					keyExtractor={item => item._id}
+					data={data} />
   </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-		backgroundColor: '#FFEB3B',
-		flex: 1,
-		flexDirection: 'row',
-	},
-	title: {
-		backgroundColor: 'orange',
-		marginVertical: 30,
-		textAlign: 'center',
-		paddingVertical: 60,
-		borderColor: 'yellow',
-		borderWidth: 2,
-		fontSize: 36,
-    width
+	container: {
+		flex: 1
 	}
 });
 
