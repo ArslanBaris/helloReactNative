e2fc9070fcb1e8fe476db4074f793bae905276c2
ArslanBaris <arslan.bariss@outlook.com>
2023-09-24 20:40:31 +0300

    ScrollView

--- App.tsx
+++ App.tsx
@@ -21,6 +21,7 @@ import {
   Platform,
   TouchableOpacity,
   TextInput,
+  Dimensions,
 } from 'react-native';
 
 import {
@@ -30,48 +31,49 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+const {width}  = Dimensions.get('window');
 
 
 function App(): JSX.Element {
-  const [name, setName] = useState("")
 
-  const _onChangeText = (e: string) => {
-    setName(e)
-  };
 
   return (
     <View style={styles.container}>
-      <Text>{name}</Text>
-      <TextInput
-      // secureTextEntry={true}
-      editable={true}
-      keyboardAppearance='light'
-        autoCapitalize='sentences'
-        // keyboardType='number-pad'
-        value={name}
-        placeholder='Bir isim girin...'
-        onChangeText={_onChangeText}
-        style={styles.myInput}
-      />
-    </View>
+    <ScrollView
+      pagingEnabled={true}
+      horizontal={true}
+    >
+      <Text style={styles.title}>1</Text>
+      <Text style={styles.title}>2</Text>
+      <Text style={styles.title}>3</Text>
+      <Text style={styles.title}>4</Text>
+      <Text style={styles.title}>5</Text>
+      <Text style={styles.title}>6</Text>
+      <Text style={styles.title}>7</Text>
+      <Text style={styles.title}>8</Text>
+      <Text style={styles.title}>9</Text>
+      <Text style={styles.title}>10</Text>
+    </ScrollView>
+  </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#FFEB3B',
-    flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingHorizontal: 10
-  },
-  myInput: {
-    width: '100%',
-    height: 60,
-    borderWidth: 2,
-    borderColor: 'gray'
-  }
+		backgroundColor: '#FFEB3B',
+		flex: 1,
+		flexDirection: 'row',
+	},
+	title: {
+		backgroundColor: 'orange',
+		marginVertical: 30,
+		textAlign: 'center',
+		paddingVertical: 60,
+		borderColor: 'yellow',
+		borderWidth: 2,
+		fontSize: 36,
+    width
+	}
 });
 
 export default App;
