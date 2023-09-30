1eb5344c4fc617e6d7c75c06cd36506c514bcbdc
ArslanBaris <arslan.bariss@outlook.com>
2023-09-30 23:07:25 +0300

    async/await request

--- App.tsx
+++ App.tsx
@@ -4,32 +4,67 @@
  *
  * @format
  */
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import type { PropsWithChildren } from 'react';
 import {
+  Button,
   SafeAreaView,
   StyleSheet,
+  Text,
 
 } from 'react-native';
 
-
+import axios from 'axios'
 import FlatListExample from './src/components/FlatListExample';
 import PlatformExample from './src/components/PlatformExample';
 
 
 function App(): JSX.Element {
 
+  const [name, setName] = useState<string>('Barış')
+  const [surname, setSurname] = useState<string>('Arslan')
+  const [loading, setLoading] = useState<boolean>(true)
+
+  useEffect(() => {
+    getRandomUser()
+  },[])
+
+  const getRandomUser = async() => {
+
+    setLoading(true)
+
+    const { data : {results} } = await axios.get("https://randomuser.me/api/")
+    const { name: { first, last } } = results[0];
+
+    
+      setName(first); 
+      setSurname(last)
+      setLoading(false)
+  }
+
   return (
     <SafeAreaView style={styles.container}>
-   	<PlatformExample />
+     {/* <FlatListExample /> */}
+     {
+						loading ? <Text style={styles.text}>Loading...</Text> :
+							<Text style={styles.text}>{name} {surname}</Text>
+					}
+					<Button
+						title={"Random User"}
+						onPress={getRandomUser} />
   </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-		flex: 1
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center'
 	},
+  text: {
+		textAlign: 'center'
+	}
 });
 
 export default App;
